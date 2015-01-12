6b1bf60ba81b52f3b0ee7ae2aeee0bee7dc4f48c
Jim Wen <hwen3@ualerta.ca>
2015-01-11 17:16:08 -0700

    finished the code and tests \ncan make code pretty if need

--- server.py
+++ server.py
@@ -3,70 +3,95 @@ import os
 import mimetypes
 from time import ctime
 
-http_res_codes = {
-	'200': "OK"
-	'404': "Not Found"
-}
-
-new_line = "\n"
-connection_closed = "Connection: close"
-accept_ranges = "Accept-Ranges: bytes"
-server_type = "Server: python"
-OK_response = "OK"
-
-def is_subdirectory(path):
-	cur_path = os.path.realpath(os.curdir)
-	checked_path = os.path.realpath(cur_path + path)
-	print ("data yo %s\n" % checked_path)
-	return checked_path.startswith(os.curdir + os.sep)
-
-def parse_path(str):
-	str_list = str.split("\n") #split get call to extract the folder/file request
-	get_str = str_list[0].split(" ")
-	return get_str[1] if len(get_str) > 1 else " /../../.." #default the check to parent folder thus returning 404 error
-
-def construct_response(code="404", path=None):
-	#response follows the following format:
-	# HTTP/1.1 200 OK
-	# Date: Sun, 18 Oct 2009 08:56:53
-	# Server: python
-	# Last-Modified: Sat, 20 Nov 2004 07:16:26 GMT
-	# Accept-Ranges: bytes
-	# Content-Length: xxx
-	# Connection: close
-	# Content-Type: text/html
-
-	status_line = "HTTP/1.1 " + code + " " + http_res_codes[code] + new_line
-	general_header = "Date: " + ctime() + new_line + connection_closed + new_line
-	response_header = accept_ranges + new_line + server_type + new_line
-	response_str = status_line + general_header + response_header
-
-	if (path != None):
-		entity_header = "Content-Type: " + Content-Type + new_line + "Content-Length: " + len(Content-Length) + new_line
-		response_str = response_str + entity_header + new_line + data
-
-	return response_str
+# coding: utf-8
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Furthermore it is derived from the Python documentation examples thus
+# some of the code is Copyright (c) 2001-2013 Python Software
+# Foundation; All Rights Reserved
+#
+# http://docs.python.org/2/library/socketserver.html
+#
+# run: python freetests.py
+# try: curl -v -X GET http://127.0.0.1:8080/
 
 
 class MyWebServer(SocketServer.BaseRequestHandler):
+
+	HTTP_RES_CODES = {
+	'200': "OK",
+	'404': "Not Found"
+	}
+
+	STATUS_FORMAT = "HTTP/1.1 {} {} \r\n"
+	GENERAL_HEADER_FORMAT = "Date: {} \r\nConnection: close \r\n"
+	RESPONSE_HEADER_FORMAT = "Accept-Ranges: bytes \r\nServer: python \r\n"
+	ENTITY_HEADER_FORMAT = "Content-Type: {} \r\nContent-Length: {} \r\n\r\n"
+	OK_RESPONSE = "200"
+	SERVER_MAIN_PATH = os.path.realpath(os.curdir) + "/www"
+
+	def is_subdirectory(self, path):
+		checked_path = os.path.realpath(self.SERVER_MAIN_PATH + path)
+		return checked_path.startswith(self.SERVER_MAIN_PATH)
+
+	def parse_path(self, str):
+		str_list = str.split("\n") #split get call to extract the folder/file request
+		get_str = str_list[0].split(" ")
+		return self.SERVER_MAIN_PATH + get_str[1] if len(get_str) > 1 else " /../../.." #default the check to parent folder thus returning 404 error
+
+	def construct_response(self, code="404", path=None):
+		#response follows the following format:
+		# HTTP/1.1 200 OK
+		# Date: Sun, 18 Oct 2009 08:56:53
+		# Server: python
+		# Last-Modified: Sat, 20 Nov 2004 07:16:26 GMT
+		# Accept-Ranges: bytes
+		# Content-Length: xxx
+		# Connection: close
+		# Content-Type: text/html
+
+		response_str = self.STATUS_FORMAT.format(code, self.HTTP_RES_CODES[code]) + \
+			self.GENERAL_HEADER_FORMAT.format(ctime()) + \
+			self.RESPONSE_HEADER_FORMAT
+
+		if (path != None):
+			mimetypes.init()
+			file_data = ""
+			content_type = mimetypes.guess_type(path)[0]
+			with open(path) as req_file:
+				file_data += req_file.read()
+			response_str += self.ENTITY_HEADER_FORMAT.format(content_type, len(file_data)) + file_data
+
+		return response_str
+
 	
 	def serve_get(self):
-		request_path = parse_path(self.data)
-		if (is_subdirectory(request_path) == False):
-			return construct_response()
+		request_path = self.parse_path(self.data)
+		if (self.is_subdirectory(request_path) == False):
+			return self.construct_response()
 		if (os.path.exists(request_path) == False):
-			return construct_response()
-		# need to throw above 404 data as error codes
+			return self.construct_response()
+		if (os.path.isdir(request_path)):
+			request_path += "index.html"
 
-		return construct_response(OK_response, request_path)
-		# also need to return actual data
+		return self.construct_response(self.OK_RESPONSE, request_path)
 
 	def handle(self):
 		self.data = self.request.recv(1024).strip()
-		print ("Got a request of: %s\n" % self.data)
-		test_data = self.serve_get()
-		print ("Data sent: %s\n" % test_data)
-		self.request.sendall(test_data)
+		self.request.sendall(self.serve_get())
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
